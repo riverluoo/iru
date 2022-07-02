cca6e0b375179366156faccc8a38e594b6262951
wangyangyang <769789170@qq.com>
2022-07-02 15:22:29 +0800

    add

--- code/demo_1.go
+++ code/demo_1.go
@@ -13,4 +13,19 @@ func main() {
 	const name3, name4 = "AA", 123
 	fmt.Println(name3, name4)
 
+	fmt.Println("-----------------------------")
+
+	var a_1 uint8 = 10
+	var a_2 = 20
+	a_3 := 30
+	fmt.Println(a_1, a_2, a_3)
+
+	fmt.Println("-----------------------------")
+
+	var a_4, a_5, a_6 int = 40, 50, 60
+	fmt.Println(a_4,a_5,a_6)
+
+	fmt.Println("-----------------------------")
+
+
 }
